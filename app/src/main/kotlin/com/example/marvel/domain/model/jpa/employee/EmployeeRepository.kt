7eaf7cd88200cdfcfe456e21538f6b8e36ccc110
soberich <25544967+soberich@users.noreply.github.com>
2019-10-09 02:22:51 +0200

    bugfix(persistence): Temporary resolution for compilation order issue.

--- app/src/main/kotlin/com/example/marvel/domain/model/jpa/employee/EmployeeRepository.kt
+++ app/src/main/kotlin/com/example/marvel/domain/model/jpa/employee/EmployeeRepository.kt
@@ -15,13 +15,13 @@ interface EmployeeRepository : JpaRepository<EmployeeEntity, Long> {
         SELECT NEW com.example.marvel.domain.model.jpa.record.RecordListingView(p.date, p.type, p.hoursSubmitted, p.desc, p.report.id)
         FROM   RecordEntity p
         JOIN   p.report c
-        WHERE  c.id    = :${RecordCollectionEntity_.ID}
-          AND  c.year  = :${RecordCollectionEntity_.YEAR}
-          AND  c.month = :${RecordCollectionEntity_.MONTH}""")
+        WHERE  c.id    = :id
+          AND  c.year  = :year
+          AND  c.month = :month""")
     fun listForPeriod(
-        @Param(RecordCollectionEntity_.ID) id: Long,
-        @Param(RecordCollectionEntity_.YEAR) year: Int,
-        @Param(RecordCollectionEntity_.MONTH) month: Month): List<RecordListingView>
+        @Param("id") id: Long,
+        @Param("year") year: Int,
+        @Param("month") month: Month): List<RecordListingView>
 
 
 }
