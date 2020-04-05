f5560cbb5ad60a63c179a35f73f0066efa9d54c8
soberich <25544967+soberich@users.noreply.github.com>
2020-04-05 21:34:41 +0200

    bugfix(docs): Move `package-info.java` to a proper location.

--- time-service/runner/src/main/java/com/example/marvel/runtime/package-info.java
+++ time-service/runner/src/main/java/com/example/marvel/runtime/package-info.java
@@ -3,4 +3,4 @@
  * Contains platform specific runtime definitions.
  * @implNote This file is in `java` sourceSet due to Quarkus bug requiring something to be in `classes/java`
  */
-package com.example.marvel.api;
+package com.example.marvel.runtime;
