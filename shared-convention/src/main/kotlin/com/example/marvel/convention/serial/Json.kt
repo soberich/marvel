5f75b90eef396ce7faf0278c87e6ba2aca1b2fc7
soberich <25544967+soberich@users.noreply.github.com>
2020-08-05 08:02:44 +0200

    chore(convention): Clean-up.

--- shared-convention/src/main/kotlin/com/example/marvel/convention/serial/Json.kt
+++ shared-convention/src/main/kotlin/com/example/marvel/convention/serial/Json.kt
@@ -3,22 +3,12 @@ package com.example.marvel.convention.serial
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.PUBLIC_ONLY
 import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_ABSENT
 import com.fasterxml.jackson.annotation.PropertyAccessor.ALL
-import com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_NUMBERS_FOR_ENUMS
-import com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES
-import com.fasterxml.jackson.databind.DeserializationFeature.READ_ENUMS_USING_TO_STRING
-import com.fasterxml.jackson.databind.MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS
-import com.fasterxml.jackson.databind.MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES
-import com.fasterxml.jackson.databind.MapperFeature.ACCEPT_CASE_INSENSITIVE_VALUES
-import com.fasterxml.jackson.databind.MapperFeature.ALLOW_EXPLICIT_PROPERTY_RENAMING
-import com.fasterxml.jackson.databind.MapperFeature.ALLOW_FINAL_FIELDS_AS_MUTATORS
-import com.fasterxml.jackson.databind.MapperFeature.DEFAULT_VIEW_INCLUSION
+import com.fasterxml.jackson.databind.DeserializationFeature.*
+import com.fasterxml.jackson.databind.MapperFeature.*
 import com.fasterxml.jackson.databind.SerializationFeature.WRITE_ENUMS_USING_TO_STRING
 import com.fasterxml.jackson.databind.json.JsonMapper
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
-import com.fasterxml.jackson.module.afterburner.AfterburnerModule
-import com.fasterxml.jackson.module.paramnames.ParameterNamesModule
 import java.time.ZoneOffset.UTC
-import java.util.TimeZone
+import java.util.*
 
 object Json {
 
@@ -28,18 +18,31 @@ object Json {
     val configuredBuilder: JsonMapper.Builder
         get() =
             JsonMapper.builder()
-                .addModules(
-                    AfterburnerModule(),
-                    JavaTimeModule(),
-                    ParameterNamesModule()
-                ).defaultTimeZone(DEFAULT_TIMEZONE)
+                .findAndAddModules()
+                .defaultTimeZone(DEFAULT_TIMEZONE)
                 .serializationInclusion(NON_ABSENT)
                 .visibility(ALL, PUBLIC_ONLY)
                 /*.setDefaultSetterInfo(empty().withValueNulls(SKIP, SKIP))*/
                 //.disable(WRITE_DATES_AS_TIMESTAMPS)
-                .enable(ALLOW_EXPLICIT_PROPERTY_RENAMING, ACCEPT_CASE_INSENSITIVE_PROPERTIES, ACCEPT_CASE_INSENSITIVE_ENUMS, ACCEPT_CASE_INSENSITIVE_VALUES)
-                .enable(FAIL_ON_NUMBERS_FOR_ENUMS, READ_ENUMS_USING_TO_STRING)
-                .enable(WRITE_ENUMS_USING_TO_STRING)
-                .disable(ALLOW_FINAL_FIELDS_AS_MUTATORS, DEFAULT_VIEW_INCLUSION)
-                .disable(FAIL_ON_UNKNOWN_PROPERTIES)
+                .enable(
+                    ACCEPT_CASE_INSENSITIVE_ENUMS,
+                    ACCEPT_CASE_INSENSITIVE_PROPERTIES,
+                    ACCEPT_CASE_INSENSITIVE_VALUES,
+                    ALLOW_EXPLICIT_PROPERTY_RENAMING
+                ).enable(
+                    ACCEPT_EMPTY_ARRAY_AS_NULL_OBJECT,
+                    ACCEPT_SINGLE_VALUE_AS_ARRAY,
+                    FAIL_ON_IGNORED_PROPERTIES,
+                    FAIL_ON_NULL_FOR_PRIMITIVES,
+                    FAIL_ON_NUMBERS_FOR_ENUMS,
+                    FAIL_ON_NUMBERS_FOR_ENUMS,
+                    FAIL_ON_READING_DUP_TREE_KEY,
+                    FAIL_ON_UNKNOWN_PROPERTIES,
+                    READ_ENUMS_USING_TO_STRING
+                ).enable(
+                    WRITE_ENUMS_USING_TO_STRING
+                ).disable(
+                    ALLOW_FINAL_FIELDS_AS_MUTATORS,
+                    DEFAULT_VIEW_INCLUSION
+                ).disable(FAIL_ON_UNKNOWN_PROPERTIES)
 }
--- time-service/runner/src/main/kotlin/com/example/marvel/runtime/Application.kt
+++ time-service/runner/src/main/kotlin/com/example/marvel/runtime/Application.kt
@@ -1,6 +1,6 @@
 package com.example.marvel.runtime
 
-import io.micronaut.runtime.Micronaut
+import io.micronaut.runtime.mnRun
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
 import io.swagger.v3.oas.annotations.info.Contact
 import io.swagger.v3.oas.annotations.info.Info
@@ -12,13 +12,9 @@ import io.swagger.v3.oas.annotations.info.License
         version = "0.0",
         description = "My API",
         license = License(name = "Apache 2.0", url = "http://foo.bar"),
-        contact = Contact(url = "http://gigantic-server.com", name = "Fred", email = "Fred@gigagantic-server.com")
-    )
-)
-object Application {
+        contact = Contact(url = "http://gigantic-server.com", name = "soberich", email = "soberich@gigagantic-server.com")))
+object Application
 
-    @JvmStatic
-    fun main(vararg args: String) {
-        Micronaut.run(Application::class.java, *args)
-    }
+fun main(vararg args: String) {
+    mnRun<Application>(*args)
 }
